7ee82af998cb4aca5f8d3567287825962c942ba9
Kha Tran <kha.tran@lrz.de>
2023-08-12 23:27:11 +0200

    changed Dockerfile and port to make deployment work

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/', api);
 
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '5200';
 app.set('port', port);
 
 const server = http.createServer(app);
