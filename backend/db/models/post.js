5e2372a5837582a12f5e107344770aad0b4b888e
Kha Tran <kha.tran@lrz.de>
2023-08-20 16:00:05 +0200

    backend: adjust postSchemaj

--- backend/db/models/post.js
+++ backend/db/models/post.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
-  topic: String,
-  title: String,
-  body: String,
+  id: string,
+  topics: string,
+  title: string,
+  body: string,
+  author: string,
+  created: {
+    type: Date,
+    default: new Date()
+  }
 });
 
 export const Post = mongoose.model("Post", postSchema);
