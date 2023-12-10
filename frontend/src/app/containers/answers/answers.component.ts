32daca78e414fc8ce098634f0ddbbb0fdb91a27e
khamui <khamui@me.com>
2023-12-10 13:55:58 +0100

    BE: answer response contains num of subanswers; FE: subanswers can be collapsed and expanded

--- frontend/src/app/containers/answers/answers.component.ts
+++ frontend/src/app/containers/answers/answers.component.ts
@@ -1,16 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input } from '@angular/core';
 import { Answer } from 'src/app/typedefs/Answer.typedef';
 import { DateAgoPipe } from '../../pipes/date-ago.pipe';
 import { DividerModule } from 'primeng/divider';
-import { NgFor } from '@angular/common';
+import { ToggleButton, ToggleButtonModule } from 'primeng/togglebutton';
+import { AnswerService } from 'src/app/services/answer.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
     selector: 'ama-answers',
     templateUrl: './answers.component.html',
     styleUrls: ['./answers.component.scss'],
     standalone: true,
-    imports: [NgFor, DividerModule, DateAgoPipe]
+    imports: [DividerModule, ToggleButtonModule, DateAgoPipe, AsyncPipe]
 })
 export class AnswersComponent {
-  @Input() answers!: Answer[]
+  @Input() answers!: Answer[] | null;
+
+  answers$!: Observable<Answer[]>;
+
+  constructor(private ans: AnswerService) {}
+
+  public toggleSubAnswers(answer: Answer) {
+    this.answers$ = this.ans.readSubAnswers$(answer._id as string)
+  }
 }
