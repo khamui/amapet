619fbd37f2e6ac337e247d6a1a60a29421e37489
khamui <khamui@me.com>
2024-01-06 22:34:57 +0100

    notifications mark as read

--- backend/controllers/controller.notifications.js
+++ backend/controllers/controller.notifications.js
@@ -1,5 +1,6 @@
 import {
   retrieveModel,
+  updateModel,
 } from "../dbaccess.js";
 import { Notification } from "../db/models/notification.js";
 
@@ -8,9 +9,21 @@ export const controllerNotifications = {
     const { _id: userId } = req.userPayload;
     try {
       const userNotifications = await retrieveModel(Notification, { userId });
-      res.status(200).json(userNotifications);
+      res.status(200).json(userNotifications.reverse());
     } catch (error) {
       res.status(500).send(error);
     }
   },
+  markAsRead: async (req, res) => {
+    const { id: notificationId } = req.params;
+    const filter = { _id: notificationId };
+    const updateExpr = { unread: false };
+
+    try {
+      const updated = await updateModel(Notification, filter, updateExpr);
+      res.status(200).json(updated);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
 };
