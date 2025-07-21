96d6072dbccdc92a9c6e1518ddb2a6902b861e27
khamui <khamui@me.com>
2025-07-21 22:10:03 +0200

    using new builder, updated primeng deps, removing deprecated primeflex, start integrating tailwindcss

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -9,16 +9,15 @@ import {
 } from '@angular/common/http';
 import { AppRoutingModule } from './app/app-routing.module';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import {
-  BrowserModule,
-  bootstrapApplication,
-} from '@angular/platform-browser';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import {
   GoogleLoginProvider,
   SocialAuthServiceConfig,
   SocialLoginModule,
 } from '@abacritt/angularx-social-login';
 import { MessageService } from 'primeng/api';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeng/themes/lara';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -53,6 +52,11 @@ bootstrapApplication(AppComponent, {
       } as SocialAuthServiceConfig,
     },
     provideAnimations(),
+    providePrimeNG({
+      theme: {
+        preset: Lara,
+      },
+    }),
     provideHttpClient(withInterceptorsFromDi()),
   ],
 }).catch((err) => console.error(err));
