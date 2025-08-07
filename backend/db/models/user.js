85317faccc98d553ddea85584a8493175f58d9bd
khamui <khamui@me.com>
2025-08-07 17:26:16 +0200

    fixes #141 - distinguish between followedCircles and not followed ones. start with starring/pinning as favorites

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -4,6 +4,9 @@ const userSchema = new Schema({
   firstname: String,
   lastname: String,
   email: String,
+  followedCircles: Array,
+  followedQuestions: Array,
+  respectPoints: Number
 });
 
 export const User = model("User", userSchema);
--- frontend/src/app/components/sidebox/sidebox.component.ts
+++ frontend/src/app/components/sidebox/sidebox.component.ts
@@ -1,13 +1,30 @@
 import { Component, Input } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { MenuModule } from 'primeng/menu';
+import { ApiService } from 'src/app/services/api.service';
+import { FollowResponse } from 'src/app/typedefs/FollowResponse.typedef';
 
 @Component({
-    selector: 'ama-sidebox',
-    templateUrl: './sidebox.component.html',
-    styleUrls: ['./sidebox.component.scss'],
-    standalone: true,
-    imports: [MenuModule]
+  selector: 'ama-sidebox',
+  templateUrl: './sidebox.component.html',
+  styleUrls: ['./sidebox.component.scss'],
+  standalone: true,
+  imports: [MenuModule, ButtonModule],
 })
 export class SideboxComponent {
   @Input() items: any;
+  @Input() context!: string;
+
+  constructor(private api: ApiService<any>) {}
+
+  public async handleFollowCircle(event: MouseEvent, item: any) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    const response = await this.api.create('follow-circle', {
+      circleName: item.label,
+    });
+    item.state.isFollowed =
+      (response as FollowResponse).result.action === 'followed';
+  }
 }
