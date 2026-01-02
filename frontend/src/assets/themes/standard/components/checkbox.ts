cc2a492dcf9c98571620110973d916c0d46ae65f
khamui <khamui@me.com>
2026-01-02 13:00:57 +0100

    adding theming

---
+++ frontend/src/assets/themes/standard/components/checkbox.ts
@@ -0,0 +1,15 @@
+export const checkboxTheme = {
+  checkbox: {
+    colorScheme: {
+      light: {
+        root: {
+          checkedBackground: '{accent.success}',
+          checkedBorderColor: '{accent.neutral}',
+        },
+        icon: {
+          checkedColor: '{teal.900}',
+        },
+      },
+    },
+  },
+};
